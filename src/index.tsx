b4c90f21d422c082ab1a92f4c7cfe3de184d3b75
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-16 20:49:19 -0300

    Config theme + i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
 import App from './App';
+import GlobalStyle from './GlobalStyle';
+import { theme } from './Theme';
+
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
