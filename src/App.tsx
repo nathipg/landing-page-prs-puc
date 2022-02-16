f7398468bbe707a0c699383b9e8705aef787601c
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-16 20:50:37 -0300

    Container + initial App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Home from './pages/Home';
+
+import { Container } from './containers';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Header />
+      <Home />
+      <Footer />
+    </Container>
+  );
 }
 
 export default App;
