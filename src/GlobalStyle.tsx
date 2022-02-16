503a8c3ddb565db7815e4cde5711823e69b28cfa
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-16 19:15:35 -0300

    Global Style

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: var(--dark-gray);
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+  #root {
+    min-width: 350px;
+  }
+`;
+
+export default GlobalStyle;
