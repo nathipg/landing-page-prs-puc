38bf1d60372fb7ddad6b4acf6238fe07b065de56
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-16 22:50:19 -0300

    Review component

---
+++ src/components/Review.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+import UserIcon from './UserIcon';
+
+interface ReviewProps {
+  name: string;
+  text: string;
+}
+
+const StyledReview = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  background-color: ${({ theme }) => theme.color.bgSecondary};
+  padding: 1rem;
+  border-radius: 0.5rem;
+
+  @media (min-width: ${({ theme }) => theme.breakPoint.lg}) {
+    flex-direction: row;
+    max-width: calc(50% - 1rem);
+  }
+`;
+
+const StyledReviewContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  span {
+    align-self: center;
+    font-weight: bold;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakPoint.lg}) {
+    span {
+      align-self: flex-start;
+    }
+  }
+`;
+
+const Review = ({ name, text }: ReviewProps) => {
+  return (
+    <StyledReview>
+      <UserIcon picture="https://github.com/nathipg.png" />
+      <StyledReviewContent>
+        <span>{name}</span>
+        <p>{text}</p>
+      </StyledReviewContent>
+    </StyledReview>
+  );
+};
+
+export default Review;
---
+++ src/components/UserIcon.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+interface UserIconProps {
+  picture: string;
+}
+
+interface StyledIconProps {
+  picture: string;
+}
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const StyledIcon = styled.div<StyledIconProps>`
+  border-radius: 100%;
+  background: ${({ picture }) => `url(${picture}) no-repeat center/100%`};
+  width: 5rem;
+  height: 5rem;
+`;
+
+const UserIcon = ({ picture }: UserIconProps) => {
+  return (
+    <StyledContainer>
+      <StyledIcon picture={picture}></StyledIcon>
+    </StyledContainer>
+  );
+};
+
+export default UserIcon;
