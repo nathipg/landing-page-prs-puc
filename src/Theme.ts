102d29079dace33020aa61bb6cfdfb657e2db3aa
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-16 22:49:35 -0300

    Add breakpoints

--- src/Theme.ts
+++ src/Theme.ts
@@ -2,5 +2,13 @@ export const theme = {
   color: {
     primary: '#DC143C',
     bgPrimary: '#F8F8FF',
+    bgSecondary: '#FFF',
+  },
+  breakPoint: {
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+    xxl: '1480px',
   },
 };
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,6 +1,14 @@
 export interface ThemeType {
   color: {
-    primary: 'string';
-    bgPrimary: 'string';
+    primary: string;
+    bgPrimary: string;
+    bgSecondary: string;
+  };
+  breakPoint: {
+    sm: string;
+    md: string;
+    lg: string;
+    xl: string;
+    xxl: string;
   };
 }
